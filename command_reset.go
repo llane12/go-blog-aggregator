9f5fa7df9bf066cf6adb66f36e863f1961c541e2
llane12 <lukelane1@gmail.com>
2025-06-18 08:52:54 +0900

    Extend reset command to clear current user from config file

--- command_reset.go
+++ command_reset.go
@@ -10,11 +10,16 @@ func handlerReset(s *state, cmd command) error {
 		return fmt.Errorf("usage: %s", cmd.name)
 	}
 
-	err := s.db.DeleteUsers(context.Background())
+	err := s.cfg.SetUser("")
+	if err != nil {
+		return fmt.Errorf("error clearing logged in user: %w", err)
+	}
+
+	err = s.db.DeleteUsers(context.Background())
 	if err != nil {
 		return fmt.Errorf("error deleting users: %w", err)
 	}
 
-	fmt.Printf("%s: database reset successfully\n", cmd.name)
+	fmt.Printf("%s: reset successful\n", cmd.name)
 	return nil
 }
