eb7711fd215fb88d3f898a13df12e833ca3e1049
llane12 <lukelane1@gmail.com>
2025-07-10 08:54:20 +0900

    Change agg command to a long-running function that continuously fetches from all the registered feeds

--- command_rss.go
+++ command_rss.go
@@ -2,21 +2,76 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"time"
+
+	"gator/internal/database"
 	"gator/rss"
 )
 
 func handlerAggregate(s *state, cmd command) error {
-	feedURL := "https://www.wagslane.dev/index.xml"
-	if len(cmd.args) == 1 {
-		feedURL = cmd.args[0]
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("usage: %s <time_between_reqs>", cmd.name)
 	}
 
-	feed, err := rss.FetchFeed(context.Background(), feedURL)
+	timeBetweenRequests, err := time.ParseDuration(cmd.args[0])
 	if err != nil {
-		return fmt.Errorf("%s: error fetching feed %s: %w", cmd.name, feedURL, err)
+		return fmt.Errorf("%s: invalid time_between_reqs value: %w", cmd.name, err)
 	}
 
-	fmt.Printf("%+v\n", *feed)
+	fmt.Printf("%s: collecting feeds every %s\n", cmd.name, timeBetweenRequests)
+	fmt.Println()
+
+	ticker := time.NewTicker(timeBetweenRequests)
+	for ; ; <-ticker.C {
+		err = scrapeFeeds(s.db)
+		if err != nil {
+			return fmt.Errorf("%s: error scraping feeds: %w", cmd.name, err)
+		}
+		fmt.Println()
+	}
+}
+
+func scrapeFeeds(db *database.Queries) error {
+	ctx := context.Background()
+
+	nextFeed, err := db.GetNextFeedToFetch(ctx)
+	if err != nil {
+		return err
+	}
+
+	err = scrapeFeed(ctx, db, nextFeed)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func scrapeFeed(ctx context.Context, db *database.Queries, feed database.Feed) error {
+	feedData, err := rss.FetchFeed(ctx, feed.Url)
+	if err != nil {
+		return err
+	}
+
+	err = db.MarkFeedFetched(ctx, database.MarkFeedFetchedParams{
+		ID: feed.ID,
+		LastFetchedAt: sql.NullTime{
+			Time:  time.Now().UTC(),
+			Valid: true,
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%s\n", feedData.Channel.Title)
+	fmt.Println("=======================")
+
+	for _, item := range feedData.Channel.Item {
+		fmt.Printf("%s\n", item.Title)
+	}
+
 	return nil
 }
--- rss/types.go
+++ rss/types.go
@@ -1,12 +1,14 @@
 package rss
 
 type RSSFeed struct {
-	Channel struct {
-		Title       string    `xml:"title"`
-		Link        string    `xml:"link"`
-		Description string    `xml:"description"`
-		Item        []RSSItem `xml:"item"`
-	} `xml:"channel"`
+	Channel RSSChannel `xml:"channel"`
+}
+
+type RSSChannel struct {
+	Title       string    `xml:"title"`
+	Link        string    `xml:"link"`
+	Description string    `xml:"description"`
+	Item        []RSSItem `xml:"item"`
 }
 
 type RSSItem struct {
